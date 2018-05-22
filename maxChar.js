1f24426b8b76167770c498b5799926f0671087f6
Mumtaz Ahmad <37632981+mumtazahmadui@users.noreply.github.com>
2018-05-22 17:17:11 +0530

    Add files via upload

--- maxChar.js
+++ maxChar.js
@@ -5,6 +5,25 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
+let arr = [1,0, 3,1,2,7];
+
+let minValue = arr[0];
+let maxValue = arr[0];
+let len = arr.length;
+
+for(let i = 0; i<len; i++) {
+  if(minValue > arr[i]) {
+   minValue = arr[i]
+  } if(maxValue < arr[i]) {
+    maxValue = arr[i]
+  }
+}
+console.log(minValue);
+console.log(maxValue);
+
+
+//second approach
+
 function maxChar(str) {
   const charMap = {};
   let max = 0;
